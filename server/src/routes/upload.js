2cd33344763a5e60745c440c17436571054396df
MelroyNoronha <melmax1208@gmail.com>
2018-12-11 19:15:24 +0530

    updated multer diskStorage options

--- server/src/routes/upload.js
+++ server/src/routes/upload.js
@@ -2,10 +2,18 @@ import express from "express";
 import multer from "multer";
 
 const router = express.Router();
-const upload = multer({ dest: "./userFiles" });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./userFiles");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + file.originalname);
+  }
+});
+const upload = multer({ storage: storage });
 
 router.post("/", upload.single("uploaded-file"), (req, res) => {
-  console.log(req.body, req.file);
+  console.log(req.body, req.file, req.file.filename);
   if (req.file) {
     res.json({ message: "fetch complete" });
   } else {
