6bf0e6d4369e81d53fedcab432add5d6eb67a2ac
MelroyNoronha <melmax1208@gmail.com>
2019-01-25 17:08:44 +0530

    verify jwt token before allowing file upload

--- server/src/routes/upload.js
+++ server/src/routes/upload.js
@@ -3,6 +3,7 @@ import multer from "multer";
 import User from "../models/User";
 import dotenv from "dotenv";
 import multerGfsStorage from "multer-gridfs-storage";
+import jwt from "jsonwebtoken";
 
 dotenv.config();
 const router = express.Router();
@@ -20,28 +21,34 @@ const storage = new multerGfsStorage({
 const upload = multer({ storage: storage });
 
 router.post("/", upload.single("uploaded-file"), (req, res) => {
-  if (req.file) {
-    User.updateOne(
-      { email: req.body.email },
-      {
-        $push: {
-          files: { filename: req.file.filename, fileId: req.file.id }
-        }
-      },
-      (err, document) => {
-        if (err) {
-          console.log(err);
-        } else {
-          res.json({
-            message: "file uploaded successfully",
-            file: req.file
-          });
-        }
+  jwt.verify(req.headers.authorization, process.env.JWT_SECRET, err => {
+    if (err) {
+      console.error(err);
+    } else {
+      if (req.file) {
+        User.updateOne(
+          { email: req.body.email },
+          {
+            $push: {
+              files: { filename: req.file.filename, fileId: req.file.id }
+            }
+          },
+          (err, document) => {
+            if (err) {
+              console.log(err);
+            } else {
+              res.json({
+                message: "file uploaded successfully",
+                file: req.file
+              });
+            }
+          }
+        );
+      } else {
+        res.json({ message: "something went wrong :(" });
       }
-    );
-  } else {
-    res.json({ message: "something went wrong :(" });
-  }
+    }
+  });
 });
 
 export default router;
