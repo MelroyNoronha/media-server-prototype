cb129d5da3bcf574f7964e14ff90d35722adfb52
MelroyNoronha <melmax1208@gmail.com>
2019-02-01 19:55:26 +0530

    Delete feature implementation. User.update() to remove deleted filId pending.

--- client/src/js/setupDeleteButtonHandlers.js
+++ client/src/js/setupDeleteButtonHandlers.js
@@ -3,44 +3,49 @@ import { showDeleteWarningModal } from "./modalController";
 
 export default () => {
   const deleteBtns = document.querySelectorAll(".delete-btn");
+  let clickedFileId;
+  let clickedFileName;
 
   deleteBtns.forEach(btn => {
     btn.addEventListener("click", e => {
       e.preventDefault();
-      const clickedFileId = e.target.parentNode.id;
-      const clickedFileName = e.target.parentNode.querySelector("h3").innerText;
+      clickedFileId = e.target.parentNode.id;
+      clickedFileName = e.target.parentNode.querySelector("h3").innerText;
 
       showDeleteWarningModal(
         `Are you sure you want to delete ${clickedFileName}?`
       );
-
-      const deleteWarnModal = document.getElementById("delete-warn-modal");
-      const yesBtn = document.getElementById("yes-btn");
-      const noBtn = document.getElementById("no-btn");
-
-      yesBtn.addEventListener("click", e => {
-        e.preventDefault();
-        deleteWarnModal.style.display = "none";
-        // showLoadingGif();
-        // fetch("http://localhost:8081/delete", {
-        //   method: "post",
-        //   headers: {
-        //     authorization: localStorage.getItem("media-server-token"),
-        //     user: localStorage.getItem("media-server-email"),
-        //     _id: clickedFileId
-        //   }
-        // })
-        //   .then(res => res.json())
-        //   .then(data => {
-        //     hideLoadingGif();
-        //     console.log(data);
-        //   });
-      });
-
-      noBtn.addEventListener("click", e => {
-        e.preventDefault();
-        deleteWarnModal.style.display = "none";
-      });
     });
+  });
+
+  const deleteWarnModal = document.getElementById("delete-warn-modal");
+  const yesBtn = document.getElementById("yes-btn");
+  const noBtn = document.getElementById("no-btn");
+
+  yesBtn.addEventListener("click", e => {
+    e.preventDefault();
+    deleteWarnModal.style.display = "none";
+
+    showLoadingGif();
+    fetch("http://localhost:8081/deleteFile", {
+      method: "post",
+      headers: {
+        authorization: localStorage.getItem("media-server-token"),
+        user: localStorage.getItem("media-server-email"),
+        _id: clickedFileId,
+        filename: clickedFileName
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        hideLoadingGif();
+        console.log(data);
+        window.location.reload();
+      });
+  });
+
+  noBtn.addEventListener("click", e => {
+    e.preventDefault();
+    deleteWarnModal.style.display = "none";
   });
 };
--- server/src/routes/deleteFile.js
+++ server/src/routes/deleteFile.js
@@ -1,9 +1,43 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import User from "../models/User";
+import Grid from "gridfs-stream";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+let gfs;
+mongoose.connection.once("open", () => {
+  gfs = Grid(mongoose.connection.db, mongoose.mongo);
+  gfs.collection("userFiles");
+});
 
 const router = express.Router();
 
 router.post("/", (req, res) => {
-  if (req) res.json({ test: " delete route works!" });
+  jwt.verify(req.headers.authorization, process.env.JWT_SECRET, err => {
+    if (err) {
+      console.error(err);
+    } else {
+      // remove file from grid store
+      gfs.remove({ _id: req.headers._id, root: "userFiles" }, err => {
+        err
+          ? console.error(err)
+          : res.json({ message: "file deleted from gridfs" });
+      });
+
+      //remove file from user record
+      User.update(
+        { email: req.headers.user },
+        { $pull: { files: { fileId: req.headers._id } } },
+        {safe: true},
+        (err, doc) => {
+          err ? console.error(err) : console.log(doc);
+        }
+      );
+    }
+  });
 });
 
 export default router;
