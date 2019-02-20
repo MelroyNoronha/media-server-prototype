fa8d07a219d3830f4b4b297665b468704076b859
MelroyNoronha <melmax1208@gmail.com>
2019-02-20 17:43:32 +0530

    fix mongoURI

--- server/src/globalConstants.js
+++ server/src/globalConstants.js
@@ -2,6 +2,5 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const mongoURI = `
-  mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}
-  `;
+export const mongoURI = 
+`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
