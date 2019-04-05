b51ddfcf329e6f1aa00109bea3243f43a5c91d32
MelroyNoronha <melmax1208@gmail.com>
2019-04-05 08:27:08 +0530

    file list now shows file size of each file

--- client/src/js/fetchAndRenderUserFileList.js
+++ client/src/js/fetchAndRenderUserFileList.js
@@ -5,11 +5,14 @@ const fileList = document.getElementById("file-list");
 
 const renderUserFileList = files => {
   files.forEach((file, index) => {
+    let fileSize = Number((file.length/1000000).toFixed(2))
+
     fileList.innerHTML += `
       <li id="${file._id}"> 
         <h3 id="${file.filename}"> ${file.filename} </h3>
         <p> <strong>Uploaded on:</strong> ${file.uploadDate}</p>
         <p> <strong>Type:</strong> ${file.contentType}</p>
+        <p> <strong>Size:</strong> ${fileSize} MB</p>
         <button id="download-btn-${index}" class="download-btn">
           Download
         </button>
